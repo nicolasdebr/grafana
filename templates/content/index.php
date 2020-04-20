1c07dc8b156af2aa75d94dc7bc187d785be2500f
nicolasdebr <56118437+nicolasdebr@users.noreply.github.com>
2020-04-20 14:25:43 +0200

    Update index.php

--- templates/content/index.php
+++ templates/content/index.php
@@ -1,2 +1,3 @@
 <iframe src="https://play.grafana.org/d/000000012/grafana-play-home?orgId=1" width="100%" height="100%" frameborder="0"></iframe>
-
+<iframe src="https://play.grafana.org/d-solo/000000012/grafana-play-home?orgId=1&from=1587378222028&to=1587385422028&panelId=5" width="450" height="200" frameborder="0"></iframe>
+<iframe src="https://play.grafana.org/d-solo/000000012/grafana-play-home?orgId=1&from=1587378324114&to=1587385524114&panelId=11" width="450" height="200" frameborder="0"></iframe>
