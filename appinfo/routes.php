50115e9c29b5cad026bac60669cef1dc3e1ae255
nicolasdebr <56118437+nicolasdebr@users.noreply.github.com>
2020-04-28 11:42:55 +0200

    Update routes.php

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -9,6 +9,10 @@
  */
 return [
     'routes' => [
-	   ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
-	   ['name' => 'note#create', 'url' => '/notes', 'verb' => 'POST'],
+	['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
+        ['name' => 'note#index', 'url' => '/notes', 'verb' => 'GET'],
+        ['name' => 'note#show', 'url' => '/notes/{id}', 'verb' => 'GET'],
+        ['name' => 'note#create', 'url' => '/notes', 'verb' => 'POST'],
+        ['name' => 'note#update', 'url' => '/notes/{id}', 'verb' => 'PUT'],
+        ['name' => 'note#destroy', 'url' => '/notes/{id}', 'verb' => 'DELETE']
 ];
