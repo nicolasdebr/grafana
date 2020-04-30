a200f9d17a379532c388b7aa0787c4a393326104
nicolasdebr <56118437+nicolasdebr@users.noreply.github.com>
2020-04-30 11:43:47 +0200

    Update NoteController.php

--- lib/Controller/NoteController.php
+++ lib/Controller/NoteController.php
@@ -1,1 +1,59 @@
+<?php
+ namespace OCA\NotesTutorial\Controller;
 
+ use OCP\IRequest;
+ use OCP\AppFramework\Controller;
+
+ class NoteController extends Controller {
+
+     public function __construct(string $AppName, IRequest $request){
+         parent::__construct($AppName, $request);
+     }
+
+     /**
+      * @NoAdminRequired
+      */
+     public function index() {
+         // empty for now
+     }
+
+     /**
+      * @NoAdminRequired
+      *
+      * @param int $id
+      */
+     public function show(int $id) {
+         // empty for now
+     }
+
+     /**
+      * @NoAdminRequired
+      *
+      * @param string $title
+      * @param string $content
+      */
+     public function create(string $title, string $content) {
+         // empty for now
+     }
+
+     /**
+      * @NoAdminRequired
+      *
+      * @param int $id
+      * @param string $title
+      * @param string $content
+      */
+     public function update(int $id, string $title, string $content) {
+         // empty for now
+     }
+
+     /**
+      * @NoAdminRequired
+      *
+      * @param int $id
+      */
+     public function destroy(int $id) {
+         // empty for now
+     }
+
+ }
